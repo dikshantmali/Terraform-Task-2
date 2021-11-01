64e598ce2e06e84460fa9a9a7d59937e664afd36
dikshant mali <47856779+dikshantmali@users.noreply.github.com>
2021-11-01 20:22:04 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 
 region = "us-east-1"
-access_key = "AKIAUQT2DDJMD6XLGJ4U"
-secret_key = "ZjsCZB1vibDfVrYYo0uvtiHz2Cd0m8cts9l81fNH"
+access_key = "your access key"
+secret_key = "your secret key"
 
 }
